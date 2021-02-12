5246d15a04a8f564a03ddc2859348db2c99ae902
Tina <tingchia1003@vt.edu>
2021-02-12 14:54:50 -0500

    init project

---
+++ RiverCrossing/src/river/GameObject.java
@@ -0,0 +1,46 @@
+package river;
+
+import river.GameEngine.Location;
+
+public class GameObject {
+
+    protected String name;
+    protected Location location;
+
+    public GameObject() {
+
+    }
+
+    public GameObject(String name, Location location){
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location loc) {
+        this.location = loc;
+    }
+
+    public String getSound() {
+        switch (name) {
+        case "Wolf":
+            return "Howl";
+        case "Goose":
+            return "Honk";
+        default:
+            return "";
+        }
+    }
+
+}
